f43b95377cc9c41bc94b7a8f16ec96841b7a62a4
akareactor <akareactor@mail.ru>
2026-06-19 13:22:38 +0300

    DeviceType

--- Runtime/Scripts/Providers/IPlatformProvider.cs
+++ Runtime/Scripts/Providers/IPlatformProvider.cs
@@ -1,8 +1,11 @@
+using UnityEngine;
+
 namespace KulibinSpace.Platform {
 
     public interface IPlatformProvider {
         string Language { get; }
         void GameReady ();
+        DeviceType deviceType { get; }
     }
 
 }
