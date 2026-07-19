41c60fab1f15e5c013262f4c0e355acbc4440533
akareactor <akareactor@mail.ru>
2026-07-19 10:45:14 +0300

    Добавлено поле rank

--- Runtime/Scripts/LeaderboardItem.cs
+++ Runtime/Scripts/LeaderboardItem.cs
@@ -1,8 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public struct LeaderboardItem {
+    public int rank; // 2026-07-19 10:43:48 номер места в таблице, ведь можно запросить произвольный диапазон
     public string name;
     public int score;
     public bool current; // является ли рекордсмен этим зареганным игроком?
